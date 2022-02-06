dc5003818c699e6ec9cb403c014d9c0e598583a2
rramprabhakar@gmail.com <rramprabhakar@gmail.com>
2022-02-06 02:14:45 -0700

    inital git commit

---
+++ src/main/java/aom/agentenroll/app/agentenroll/AgentenrollApplication.java
@@ -0,0 +1,14 @@
+package aom.agentenroll.app.agentenroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AgentenrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentenrollApplication.class, args);
+	}
+
+}
