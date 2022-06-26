66a43981e0762d9610cb3fc4bf926e5db3499a4c
rramprabhakar@gmail.com <rramprabhakar@gmail.com>
2022-06-26 14:17:39 -0700

    commiting code

--- src/main/java/aom/agentenroll/app/agentenroll/AgentenrollApplication.java
+++ src/main/java/aom/agentenroll/app/agentenroll/AgentenrollApplication.java
@@ -10,5 +10,6 @@ public class AgentenrollApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AgentenrollApplication.class, args);
 	}
-
+//	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
+//	git remote set-url origin https://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@github.com/rramprabhakar/inbox-app.git
 }
